93a04ee24692f675be0a6c7192a24224a3463c6b
Linsey R <linsey@outlook.com>
2013-07-04 15:29:50 +0200

    getUserActions() function; UserAction class

--- src/ApiWrapper.py
+++ src/ApiWrapper.py
@@ -2,10 +2,53 @@
 
 # Author: Linsey Raymaekers
 # Copyright OKFN Belgium
+# 
+# This class is a wrapper for the VikingSpots API.
+#
+
+import json
+import requests     # HTTP library
 
 class ApiWrapper:
     name = "Mobile Vikings API Wrapper"
+    token = ""
+    userActionRequest = "https://alpha.vikingspots.com/en/api/4/users/actions/"
 
-    def helloWorld(self):
-        print "Hello World!"
 
+    # Constructor
+    def __init__(self):
+        file = open("vikingtoken")
+        fileContents = file.read()
+        entries = fileContents.split("=")
+        self.token = entries[1]
+
+
+    # Returns array of user actions
+    def getUserActions(self):
+        url = self.userActionRequest
+        params = dict(
+            bearer_token = self.token,
+            max = 5
+        )
+        resp = requests.get(url, params=params, verify=False)
+        jsonData = resp.json()
+        userActions = jsonData["response"]["items"]
+        return userActions
+
+
+class UserAction():
+    created_on = ""
+    id = 0
+    is_first = False
+    points = 0
+    type = ""
+    used_id = 0
+
+    def __init__(self, created_on, id, is_first, points, type, user_id):
+        self.created_on = created_on
+        self.id = id
+        self.is_first = is_first
+        self.points = points
+        self.type = type
+        self.user_id = user_id
+        
--- src/test.py
+++ src/test.py
@@ -8,7 +8,12 @@
 import ApiWrapper
 
 print "Testing..."
+
 api = ApiWrapper.ApiWrapper()
-print api.name
-api.helloWorld()
+
+userActions = api.getUserActions()
+print userActions[0]
+
+userAction = ApiWrapper.UserAction("1", 1, True, 200, "UserRegistered", 1000489)
+print userAction.type
 
