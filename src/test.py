adf50384364d69e438a39490eb69960291871f72
Linsey R <linsey@outlook.com>
2013-07-05 12:42:16 +0200

    Renamed ApiWrapper->VikingSpotsApiWrapper; Created WhatsNextApi module and added existing functions to it (getDayCheckins() etc)

--- src/test.py
+++ src/test.py
@@ -6,59 +6,21 @@
 # Testing class
 #
 ###################################
-import ApiWrapper
-from ApiWrapper import *
-import time                 # occurredInLast24Hours()
-import datetime             # occurredInLast24Hours()
+import WhatsNextApi
+from WhatsNextApi import *
 ###################################
 
-
-api = ApiWrapper()
-
-
-####################################
-# Functions   
-####################################
-
-def retrieveCheckinsFromActions(userActions):
-    checkins = list()
-    for action in userActions:
-        if "Checkin" == action.type:    # TODO use correct VikingSpots type for user checkin
-            checkins.append(action)
-    return checkins
-
-# created_on format: %Y-%m-%d %H:%M:%S
-def occurredInLast24Hours(checkin):
-    timeInSec = time.time()
-    time24HoursAgo = timeInSec - 24 * 60 * 60
-    timeStr = datetime.datetime.fromtimestamp(time24HoursAgo).strftime('%Y-%m-%d %H:%M:%S')
-    if timeStr < checkin.created_on:
-        return True
-    return False
-
-# Timespan specifies how far back the user actions go
-# E.g. timespan = 24 returns user actions made in the last 24 hours
-# TODO: Function retrieves a set, limited amount of user actions for now,
-#       should keep asking for more data until actions go further back 
-#       than the given timespan.
-def getDayCheckins():
-    userActions = api.getUserActions(20)
-    checkins = retrieveCheckinsFromActions(userActions)
-    dayCheckins = list()
-    for checkin in checkins:
-        if occurredInLast24Hours(checkin):
-            dayCheckins.append(checkin)
-    return dayCheckins
 
 
 
 ####################################
 # Test Code   
 ####################################
-
 print "Testing..."
 
-checkins = getDayCheckins()
+api = WhatsNextApi()
+checkins = api.getDayCheckins()
+print len(checkins)
 
 spotIds = list()
 for action in checkins:
