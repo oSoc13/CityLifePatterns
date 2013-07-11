f58e90aec55d1aeaf08d4a0c2c33fef9c12ba785
Wouter Vandenneucker <woutervddn@gmail.com>
2013-07-11 08:23:18 +0200

    included / fixed the implementation of the "whatsnext"-page

--- src/extension/inject.js
+++ src/extension/inject.js
@@ -13,18 +13,12 @@
        if(components.view == "NextSpots" && lastComponentView == "SpotDetail"){
             var spotID = components.spotID;
             setTimeout(function(){
-               /*     <div class=\"listitem more\">
-                        <a class=\"morebutton color-f01558\" data-href=\"params%5Bchannel%5D=shoppingplaces&amp;type=list&amp;channel=builtin&amp;view=Discover\" onclick=\"app.navigate('params%5Bchannel%5D=shoppingplaces&amp;type=list&amp;channel=builtin&amp;view=Discover'); return false;\">
-                            <img src=\"https://vikingspots.com/s/v-186ca808//citylife/img/icons/white/shopping.png\" class=\"icon\" alt=\"shopping\"></a><a class=\"title\" data-href=\"params%5Bid%5D=10784&amp;type=spotdetail&amp;channel=spots&amp;view=SpotDetail\" onclick=\"app.navigate('params%5Bid%5D=10784&amp;type=spotdetail&amp;channel=spots&amp;view=SpotDetail'); return false;\">
-                            <div class=\"vertically-centered has-description\">
-                                <h1>Wheels and more</h1>
-                                <p>1km — Banden Eric</p>
-                            </div>
-                        </a>
-                    </div> */
                 $("header .navigation .title").html("What's up Next?");
                whatsUpNext(spotID);
             }, 1500);
+       }
+       if(lastComponentView == "NextSpots"){
+            $(".page.list .contents").html("");
        }
        lastComponentView = components.view;
 });
@@ -35,7 +29,7 @@ function URLToArray(url) {
   var pairs = url.substring(url.indexOf('#') + 1).split('&');
   for (var i = 0; i < pairs.length; i++) {
     var pair = pairs[i].split('=');
-    if (i==0){
+    if(pair.indexOf("params[id]") != -1){
         request["spotID"] = decodeURIComponent(pair[1]);
     }else{
         request[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
@@ -46,45 +40,52 @@ function URLToArray(url) {
 
 function whatsUpNext(spotID){
     $(".page.list .contents").html("");
+    if(userToken != ""){
+        requestJson(userToken, spotID);
+    }else{
+        function main () {
+            window.postMessage({ type: "FROM_PAGE", token: config.token }, "*");
+        }
+        var script = document.createElement('script');
+        script.appendChild(document.createTextNode('('+ main +')();'));
+        (document.body || document.head || document.documentElement).appendChild(script);
+        var userToken="";
     
-    function main () {
-        window.postMessage({ type: "FROM_PAGE", token: config.token }, "*");
-        console.log("page token: "+event.data.token);
-    }
-    var script = document.createElement('script');
-    script.appendChild(document.createTextNode('('+ main +')();'));
-    (document.body || document.head || document.documentElement).appendChild(script);
-    
-    window.addEventListener("message", function(event) {
+        window.addEventListener("message", function(event) {
         // We only accept messages from ourselves
         if (event.source != window)
           return;
 
         if (event.data.type && (event.data.type == "FROM_PAGE")) {
-            console.log("script token: "+event.data.token);
-            requestJson(event.data.token, spotID);
+            userToken = event.data.token;
+            requestJson(userToken, spotID);
         }
     }, false);
+    }
+    
 }
-
 function requestJson(token, spotID){
-    var whatsNextUrl="http://linseysAPI.be/api/"+token+"/whatsnext/"+spotID+"/"
-    window.alert(whatsNextUrl);
-    /*$.getJSON( whatsNextUrl, function(data) {
-        $.each(data, function(key, val) {
-        });
-    });*/
-    for(var i=0;i<5;i++){
-        var html;
-        html = "<div class=\"listitem more\">";
-        html += "<a class=\"morebutton color-f01558\" data-href=\"params%5Bchannel%5D=shoppingplaces&amp;type=list&amp;channel=builtin&amp;view=Discover\" onclick=\"app.navigate('params%5Bchannel%5D=shoppingplaces&amp;type=list&amp;channel=builtin&amp;view=Discover'); return false;\">";
-        html += "<img src=\"https://vikingspots.com/s/v-186ca808//citylife/img/icons/white/shopping.png\" class=\"icon\" alt=\"shopping\"></a><a class=\"title\" data-href=\"params%5Bid%5D=10784&amp;type=spotdetail&amp;channel=spots&amp;view=SpotDetail\" onclick=\"app.navigate('params%5Bid%5D=10784&amp;type=spotdetail&amp;channel=spots&amp;view=SpotDetail'); return false;\">";
-        html += "<div class=\"vertically-centered has-description\">";
-        html += "<h1>"+spotID+"</h1>";
-        html += "<p>1km — Banden Eric</p>";
-        html += "</div>";
-        html += "</a>";
-        html += "</div>";
-        $(".page.list .contents").append(html);
+    var whatsNextUrl="http://127.0.0.1:8000/api/"+token+"/whatsnext/"+spotID+"/";
+    $.ajax({
+        dataType: "json",
+        url: whatsNextUrl,
+        success: handleResponse
+    });
+    
+    function handleResponse(data) {
+        theSpots = data.response.spots;
+        var html="";
+        for(var i=0;i<theSpots.length;i++){
+            html += "<div class=\"listitem more\">";
+            html += "<a class=\"morebutton color-39c3ae\" data-href=\"params%5Bchannel%5D=shoppingplaces&amp;type=list&amp;channel=builtin&amp;view=Discover\" onclick=\"app.navigate('params%5Bchannel%5D=shoppingplaces&amp;type=list&amp;channel=builtin&amp;view=Discover'); return false;\">";
+            html += "<img src=\"http://vdnkr.be/icon-next.png\" class=\"icon\" alt=\"shopping\"></a><a class=\"title\" data-href=\"params%5Bid%5D="+theSpots[i].id+"&amp;type=spotdetail&amp;channel=spots&amp;view=SpotDetail\" onclick=\"app.navigate('params%5Bid%5D="+theSpots[i].id+"&amp;type=spotdetail&amp;channel=spots&amp;view=SpotDetail'); return false;\">";
+            html += "<div class=\"vertically-centered has-description\">";
+            html += "<h1>"+theSpots[i].name+"</h1>";
+            html += "<p>"+theSpots[i].description+"</p>";
+            html += "</div>";
+            html += "</a>";
+            html += "</div>";
+        }
+        $(".page.list .contents").html(html);
     }
 }
