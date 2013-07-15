2d9dd86bb0e6da5d2b5e1a26ece01c1ad0bc3d2b
Linsey R <linsey@outlook.com>
2013-07-15 22:56:21 +0200

    Fixed hideous bug /stillsobbing

--- src/modules/writeToDb.py
+++ src/modules/writeToDb.py
@@ -31,7 +31,6 @@ def write( spotMapping ):
 
 def writeToDbNew(rows):
     DBQuery.openConnection()
-
     for (spotId, nextSpotId) in rows:
         key = (spotId, nextSpotId)
         values = rows[key]
@@ -44,26 +43,31 @@ def writeToDbNew(rows):
         MtimeSpent = values['MtimeSpent']
         weightedPopularity = values['weightedPopularity']
 
-        SELECT = 'SELECT totalCount, COUNT(*) AS "Exists" ' \
-                 'FROM whatsnext WHERE spotId = %s AND nextSpotId = %s' % (spotId, nextSpotId)
+        SELECT = "SELECT totalCount, COUNT(*) AS 'Exists' " \
+                 "FROM whatsnext WHERE spotId = '%s' AND nextSpotId = '%s';" % (spotId, nextSpotId)
+
+        
         EXISTArray = DBQuery.queryDBSingleResponse(SELECT)
-        if EXISTArray[1] == 1:
-            totalCount = EXISTArray[0] + dayCount
+        dbCount = EXISTArray[0]
+        exists = EXISTArray[1]
+        if exists:
+            totalCount = dbCount + dayCount
+            print "%d = %d + %d" % (totalCount, dbCount, dayCount)
             QUERY = "UPDATE whatsnext " \
-                    "SET totalCount = '%s' AND spotCreationDate = '%s' AND lastOccurrence = '%s' " \
-                    "AND variance = '%s' AND averageTimeSpent = '%s' " \
-                    "AND MspotAge = '%s' AND MtimeSpent = '%s' AND weightedPopularity = '%s'" \
-                    "WHERE spotId=%s AND nextSpotId=%s;" % \
+                    "SET totalCount = '%s', spotCreationDate = '%s', lastOccurrence = '%s', " \
+                    "variance = '%s', averageTimeSpent = '%s', " \
+                    "MspotAge = '%s', MtimeSpent = '%s', weightedPopularity = '%s' " \
+                    "WHERE spotId = '%s' AND nextSpotId = '%s';" % \
                         (totalCount, spotCreationDate, lastOccurrence, variance, averageTimeSpent,
                          MspotAge, MtimeSpent, weightedPopularity,
                          spotId, nextSpotId)
+            print QUERY
         else:
             QUERY = "INSERT INTO whatsnext " \
                     "VALUES (NULL,  '%s',  '%s', '%s',  '%s', '%s', '%s', " \
-                    "'%s', '%s', '%s', '%s');" % \
-                        (spotId, nextSpotId, dayCount, spotCreationDate, lastOccurrence,
+                    "'%s', '%s', '%s', '%s');" %  (spotId, nextSpotId, 
+                         dayCount, spotCreationDate, lastOccurrence,
                          variance, averageTimeSpent, 
                          MspotAge, MtimeSpent, weightedPopularity)
-        #print QUERY
         DBQuery.writeDB(QUERY)
     DBQuery.closeConnection()
