4b210aea298dc7cfa2f5cc135faed0d9a5c861f2
Wouter Vandenneucker <woutervddn@gmail.com>
2013-07-18 18:01:36 +0200

    changed number of days to 7

--- src/modules/DatabaseBuilder.py
+++ src/modules/DatabaseBuilder.py
@@ -49,7 +49,7 @@ class DatabaseBuilder():
         DBQuery.closeConnection();
 
         # TODO change this so it spans all checkins
-        nrDays = 120
+        nrDays = 7
         for n in range(nrDays):
             date1 = dt1.strftime("%Y-%m-%d %H:%M:%S")
             date2 = dt2.strftime("%Y-%m-%d %H:%M:%S")
